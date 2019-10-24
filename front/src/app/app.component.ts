768a531df924b659507cc7bd5a2a6038e4d6f78f
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2019-10-24 17:02:20 -0300

    add angular template

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,17 +1,11 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { TestService } from './services/test.service';
-import { Observable } from 'rxjs';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'front';
-  msg: Observable<any> = this.test.test();
-  constructor(private test: TestService){}
+export class AppComponent {
 
-  ngOnInit(){ }
 }
