bce4a9b77788abc4e09f44b8c1ab3d9bba6db205
Joseshi <jose.nuez96@gmail.com>
2020-03-13 19:23:13 -0300

    Css to login

--- application/modules/login/views/login.php
+++ application/modules/login/views/login.php
@@ -5,12 +5,13 @@
   <script type="text/javascript" src="<?= base_url('../js/jquery-3.4.1.js'); ?>"></script>
   <script type="text/javascript" src="<?= base_url() ?>../js/bs/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="<?= base_url() ?>../css/bs/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url() ?>../css/login.css">
 </head>
 <body>
 	<div class="container p-5">
 		<div class="row">
 			<div class="col-md-4 offset-md-4">
-				<div class="card">
+				<div class="card shadow p-3 mb-5 bg-white rounded">
 					<div class="card-header">
 						<h2>Login</h2>
 					</div>
@@ -21,11 +22,11 @@
 									<label>Email</label> 
 									<input id = "email" class="form-control" type="email" name="email" placeholder="Ej: ejemplo@correo.com">
 								</div>
-								<div>
+								<div style="margin-top: 10px">
 									<label>Contraseña</label> 
 									<input class="form-control" type="password" name="password">
 								</div>
-								<input class="btn btn-primary" type="submit" value="Login">
+								<button class="btn btn-primary" id="btnLogin" type="submit">Ingresar</button>
 							</form>
 						</div>
 					</div>
