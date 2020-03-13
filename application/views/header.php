828c5b020b6fc5069766628a0eb6b20e82b7ecdb
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2020-03-12 22:26:26 -0300

    feat(header links): add links to header

--- application/views/header.php
+++ application/views/header.php
@@ -73,7 +73,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <body style="margin: 0px">
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Tecnologías Web</a>
+  <a class="navbar-brand" href="<?=base_url()?>dashboard">Tecnologías Web</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -81,7 +81,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="<?=base_url()?>dashboard">Home <span class="sr-only">(current)</span></a>
       </li>
 	</ul>
 	<div class="dropdown">
@@ -90,7 +90,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 		  <i class="fas fa-user"></i>
 	</button>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Cerrar sesión
+          <a class="dropdown-item" href="<?=base_url()?>login">Cerrar sesión
 			  <i class="fas fa-sign-out-alt"></i>
 		  </a>
         </div>
