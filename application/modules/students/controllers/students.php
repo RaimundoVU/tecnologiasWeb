91891876d288d834bed5b79261baecb19468b762
Roberto Ureta <roberto.ureta.14@gmail.com>
2020-03-30 23:49:22 -0300

    feat(Student_list): added excel import

--- application/modules/students/controllers/students.php
+++ application/modules/students/controllers/students.php
@@ -55,6 +55,79 @@ class students extends MY_Controller {
 		return;
 	}
 
+	public function excel_upload()
+	{
+		$subject_id = $_POST['subject_id'];
+		if(!empty($_FILES['file']['name'])) { 
+			// get file extension
+			$extension = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+			if($extension == 'csv'){
+				$reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
+			} elseif($extension == 'xlsx') {
+				$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+			} else {
+				$reader = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
+			}
+			// file path
+			$spreadsheet = $reader->load($_FILES['file']['tmp_name']);
+			$allDataInSheet = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+		
+			// array Count
+			$arrayCount = count($allDataInSheet);
+			$flag = 0;
+			$createArray = array('matricula', 'nombre', 'apellido_paterno', 'apellido_materno');
+			$makeArray = array('matricula' => 'matricula', 'nombre' => 'nombre', 'apellido_paterno' => 'apellido_paterno', 'apellido_materno' => 'apellido_materno');
+			$SheetDataKey = array();
+			foreach ($allDataInSheet as $dataInSheet) {
+				foreach ($dataInSheet as $key => $value) {
+					if (in_array(trim($value), $createArray)) {
+						$value = preg_replace('/\s+/', '', $value);
+						$SheetDataKey[trim($value)] = $key;
+					} 
+				}
+			}
+			$dataDiff = array_diff_key($makeArray, $SheetDataKey);
+			if (empty($dataDiff)) {
+				$flag = 1;
+			}
+			// match excel sheet column
+			if ($flag == 1) {
+				for ($i = 1; $i <= $arrayCount; $i++) {
+					$matricula = $SheetDataKey['matricula'];
+					$nombre = $SheetDataKey['nombre'];
+					$apellido_paterno = $SheetDataKey['apellido_paterno'];
+					$apellido_materno = $SheetDataKey['apellido_materno'];
+					
+					$matricula = filter_var(trim($allDataInSheet[$i][$matricula]), FILTER_SANITIZE_STRING);
+					$nombre = filter_var(trim($allDataInSheet[$i][$nombre]), FILTER_SANITIZE_STRING);
+					$apellido_paterno = filter_var(trim($allDataInSheet[$i][$apellido_paterno]), FILTER_SANITIZE_STRING);
+					$apellido_materno = filter_var(trim($allDataInSheet[$i][$apellido_materno]), FILTER_SANITIZE_STRING);
+					if (strlen($matricula)>0 && strcmp($matricula, "matricula")!=0) {
+						$fetchData = array('matricula' => $matricula, 'nombre' => $nombre, 'apellido_paterno' => $apellido_paterno, 'apellido_materno' => $apellido_materno);
+						$data = [
+							'id_estudiante' => $matricula,
+							'id_instancia_asignatura' => $subject_id
+						];
+						if(!$this->student_model->check($matricula)){
+							if ($this->student_model->check_student_in_subject($data)) {
+								$this->student_model->add_student_in_subject($data);
+								$this->student_model->add_nota($subject_id, $matricula);
+							}
+						}
+						else{
+							$this->student_model->add_student($fetchData);
+							$this->student_model->add_student_in_subject($data);
+							$this->student_model->add_nota($subject_id, $matricula);
+						}	
+					}
+				}
+			} else {
+				return "Please import correct file, did not match excel sheet column";
+			}
+		}
+		return "Alumnos agregados correctamente";
+	}
+
 	public function add_student_in_subject($id_asig, $matricula)
 	{
 		$data = [
